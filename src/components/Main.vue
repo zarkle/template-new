1fd0c4b33e974cf7db0186106c63662dfc86854c
Beverly Pham <beverly.pham@gmail.com>
2019-08-05 14:55:35 -0700

    add a card component to main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,23 +1,31 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <h2>{{ description }}</h2>
-    <div class="three-columns">
-      <p class="column">Info1</p>
-      <p class="column">Info2</p>
-      <p class="column">Info3</p>
-    </div>
+<div>
+  <h1>{{ title }}</h1>
+  <h2>{{ description }}</h2>
+  <div class="three-columns">
+    <p class="column">Info1</p>
+    <p class="column">Info2</p>
+    <p class="column">Info3</p>
   </div>
+
+  <h2>Another heading</h2>
+  <Card></Card>
+</div>
 </template>
 
 <script>
-  export default {
-    name: "Main",
-    data() {
-      return {
-      title: 'This is the Title',
-      description: 'This is a description'
-      }
+import Card from './Card.vue';
+
+export default {
+  name: "Main",
+  data() {
+    return {
+    title: 'This is the Title',
+    description: 'This is a description'
     }
-  };
+  },
+  components: {
+    Card,
+  },
+};
 </script>
